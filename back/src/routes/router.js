a8826b4713648235fae34342b3d344d843ca085f
Weslley Vieira <wesllivs@gmail.com>
2025-10-31 12:08:29 -0300

    fix: remove bookmark functionality and related routes

--- back/src/routes/router.js
+++ back/src/routes/router.js
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import authRouter from "./authRouter.js";
-import bookmarkRouter from "./bookmarkRouter.js";
 import listRouter from "./listRouter.js";
 import userRouter from "./userRouter.js";
 import tmdbRouter from "./tmdbRouter.js";
@@ -8,7 +7,6 @@ import tmdbRouter from "./tmdbRouter.js";
 const router = Router();
 
 router.use("/auth", authRouter);
-router.use("/me/bookmarks", bookmarkRouter);
 router.use("/me/lists", listRouter);
 router.use("/me", userRouter);
 router.use("/tmdb", tmdbRouter);
